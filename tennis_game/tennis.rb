f14987a0ef7a29ebb378ff267dfc1cccf95825b8
Karla Rosas <karla.rosas@michelada.io>
2022-07-19 10:47:23 -0500

    Refactorization of TennisGame1

---
+++ tennis_game/tennis.rb
@@ -0,0 +1,222 @@
+# frozen_string_literal: true
+
+require 'debug'
+
+# Version 1 to resolve tennis game kata
+class TennisGame1
+  DRAW_MESSAGES = {
+    0 => 'Love-All',
+    1 => 'Fifteen-All',
+    2 => 'Thirty-All'
+  }.freeze
+
+  WIN_MESSAGES = {
+    0 => 'Love',
+    1 => 'Fifteen',
+    2 => 'Thirty',
+    3 => 'Forty'
+  }.freeze
+
+  def initialize(player1_name, player2_name)
+    @player1_name = player1_name
+    @player2_name = player2_name
+    @p1_points = 0
+    @p2_points = 0
+  end
+
+  def won_point(player_name)
+    player_name == 'player1' ? @p1_points += 1 : @p2_points += 1
+  end
+
+  def score
+    return DRAW_MESSAGES.fetch(@p1_points, 'Deuce') if @p1_points == @p2_points
+    return result_message(@p1_points - @p2_points) if @p1_points >= 4 || @p2_points >= 4
+
+    calculate_winning_score
+  end
+
+  def result_message(result)
+    case result
+    when 1
+      "Advantage #{@player1_name}"
+    when -1
+      "Advantage #{@player2_name}"
+    when ->(n) { n >= 2 }
+      "Win for #{@player1_name}"
+    else
+      "Win for #{@player2_name}"
+    end
+  end
+
+  def calculate_winning_score
+    result = ''
+    (1...3).each do |i|
+      next result += WIN_MESSAGES[@p1_points] unless i != 1
+
+      result += "-#{WIN_MESSAGES[@p2_points]}"
+    end
+    result
+  end
+end
+
+class TennisGame2
+  def initialize(player1Name, player2Name)
+    @player1Name = player1Name
+    @player2Name = player2Name
+    @p1points = 0
+    @p2points = 0
+  end
+      
+  def won_point(playerName)
+    if playerName == @player1Name
+      p1Score()
+    else
+      p2Score()
+    end
+  end
+
+  def score
+    result = ""
+    if (@p1points == @p2points and @p1points < 3)
+      if (@p1points==0)
+        result = "Love"
+      end
+      if (@p1points==1)
+        result = "Fifteen"
+      end
+      if (@p1points==2)
+        result = "Thirty"
+      end
+      result += "-All"
+    end
+    if (@p1points==@p2points and @p1points>2)
+        result = "Deuce"
+    end
+    
+    p1res = ""
+    p2res = ""
+    if (@p1points > 0 and @p2points==0)
+      if (@p1points==1)
+        p1res = "Fifteen"
+      end
+      if (@p1points==2)
+        p1res = "Thirty"
+      end
+      if (@p1points==3)
+        p1res = "Forty"
+      end
+      p2res = "Love"
+      result = p1res + "-" + p2res
+    end
+    if (@p2points > 0 and @p1points==0)
+      if (@p2points==1)
+        p2res = "Fifteen"
+      end
+      if (@p2points==2)
+        p2res = "Thirty"
+      end
+      if (@p2points==3)
+        p2res = "Forty"
+      end
+      
+      p1res = "Love"
+      result = p1res + "-" + p2res
+    end
+    
+    if (@p1points>@p2points and @p1points < 4)
+      if (@p1points==2)
+        p1res="Thirty"
+      end
+      if (@p1points==3)
+        p1res="Forty"
+      end
+      if (@p2points==1)
+        p2res="Fifteen"
+      end
+      if (@p2points==2)
+        p2res="Thirty"
+      end
+      result = p1res + "-" + p2res
+    end
+    if (@p2points>@p1points and @p2points < 4)
+      if (@p2points==2)
+        p2res="Thirty"
+      end
+      if (@p2points==3)
+        p2res="Forty"
+      end
+      if (@p1points==1)
+        p1res="Fifteen"
+      end
+      if (@p1points==2)
+        p1res="Thirty"
+      end
+      result = p1res + "-" + p2res
+    end
+    if (@p1points > @p2points and @p2points >= 3)
+      result = "Advantage " + @player1Name
+    end
+    if (@p2points > @p1points and @p1points >= 3)
+      result = "Advantage " + @player2Name
+    end
+    if (@p1points>=4 and @p2points>=0 and (@p1points-@p2points)>=2)
+      result = "Win for " + @player1Name
+    end
+    if (@p2points>=4 and @p1points>=0 and (@p2points-@p1points)>=2)
+      result = "Win for " + @player2Name
+    end
+    result
+  end
+
+  def setp1Score(number)
+    (0..number).each do |i|
+        p1Score()
+    end
+  end
+
+  def setp2Score(number)
+    (0..number).each do |i|
+      p2Score()
+    end
+  end
+
+  def p1Score
+    @p1points +=1
+  end
+  
+  def p2Score
+    @p2points +=1
+  end
+end
+
+class TennisGame3
+  def initialize(player1Name, player2Name)
+    @p1N = player1Name
+    @p2N = player2Name
+    @p1 = 0
+    @p2 = 0
+  end
+      
+  def won_point(n)
+    if n == @p1N
+        @p1 += 1
+    else
+        @p2 += 1
+    end
+  end
+  
+  def score
+    if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6)
+      p = ["Love", "Fifteen", "Thirty", "Forty"]
+      s = p[@p1]
+      @p1 == @p2 ? s + "-All" : s + "-" + p[@p2]
+    else
+      if (@p1 == @p2)
+        "Deuce"
+      else
+        s = @p1 > @p2 ? @p1N : @p2N
+        (@p1-@p2)*(@p1-@p2) == 1 ? "Advantage " + s : "Win for " + s
+      end
+    end
+  end
+end
