72dc38d636488515421f187373ba368ab8a92841
Karla Rosas <karla.rosas@michelada.io>
2022-07-20 18:45:20 -0500

    Second version dot_calculator

--- dot_calculator/dot_calculator.rb
+++ dot_calculator/dot_calculator.rb
@@ -3,43 +3,15 @@
 # Class to calculate
 class DotCalculator
   def self.dot_calculator(equation)
-    factors = equation.split
-    sign = factors[1]
-    operand1 = factors[0].chars.count
-    operand2 = factors[2].chars.count
-    calculate(operand1, sign, operand2)
+    @operation = equation.split
+    '.' * value(0).public_send(operator, value(2))
   end
 
-  def self.calculate(operand1, operation, operand2)
-    case operation
-    when '+'
-      sum(operand1, operand2)
-    when '-'
-      substract(operand1, operand2)
-    when '//'
-      divide(operand1, operand2)
-    when '*'
-      multiply(operand1, operand2)
-    end
+  def self.operator
+    @operation[1] == '//' ? '/' : @operation[1]
   end
 
-  def self.sum(first_value, second_value)
-    result = first_value + second_value
-    '.' * result
-  end
-
-  def self.substract(first_value, second_value)
-    result = first_value - second_value
-    '.' * result
-  end
-
-  def self.divide(first_value, second_value)
-    result = first_value / second_value
-    '.' * result
-  end
-
-  def self.multiply(first_value, second_value)
-    result = first_value * second_value
-    '.' * result
+  def self.value(index)
+    @operation[index].chars.count
   end
 end
